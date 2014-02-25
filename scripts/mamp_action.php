a9bbe1247753fb04069793c128c8767cd1979bfc
khaliqgant <khaliqgant@gmail.com>
2014-02-25 18:36:34 -0500

    add in support for mamp to open the domain in the browser

--- scripts/mamp_action.php
+++ scripts/mamp_action.php
@@ -7,8 +7,16 @@ $home = $workflow->home();
 $args = $argv[1];
 $args = str_replace(" ","",$args);
 
-$mamp_hosts_path = $home."/Library/Application Support/appsolute/MAMP PRO/conf/httpd.conf";
-$lineNumber = getLineWithString($mamp_hosts_path,$args);
+$mamp_hosts_path = $home."/Library/Application Support/appsolute/MAMP PRO/httpd.conf";
+if (file_exists($mamp_hosts_path))
+{
+    $lineNumber = getLineWithString($mamp_hosts_path,$args);
+    $lineNumber = $lineNumber - 3;
+    $desiredLine = grabLine($mamp_hosts_path,$lineNumber);
+    $server = str_replace("ServerName ","",$desiredLine);
+    $server = trim($server);
+    shell_exec("open http://$server");
+}
 
 function getLineWithString($fileName, $str) {
     //http://stackoverflow.com/questions/9721952/search-string-and-return-line-php
@@ -19,6 +27,15 @@ function getLineWithString($fileName, $str) {
         }
     }
     return -1;
+}
+
+function grabLine($fileName,$Dline){
+    $lines = file($fileName);
+    foreach ($lines as $lineNumber => $line){
+        if ($lineNumber == $Dline){
+            return $line;
+        }
+    }
 }
 
 
