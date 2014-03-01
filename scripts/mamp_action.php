706116c227439a53ee57c1fa8c9fb00fbffc8ae1
khaliqgant <khaliqgant@gmail.com>
2014-02-28 22:24:15 -0500

    widen range for mamp domain lookup and improve logic

--- scripts/mamp_action.php
+++ scripts/mamp_action.php
@@ -5,22 +5,25 @@ $workflow = new Workflows();
 $home = $workflow->home();
 
 $args = $argv[1];
+$args = explode(" ",$args);
+$args = $args[0];
+
 $args = str_replace(" ","",$args);
 
 $mamp_hosts_path = $home."/Library/Application Support/appsolute/MAMP PRO/httpd.conf";
 if (file_exists($mamp_hosts_path))
 {
-    $lineNumber = getLineWithString($mamp_hosts_path,$args);
-    $lineNumber = $lineNumber - 3;
-    $desiredLine = grabLine($mamp_hosts_path,$lineNumber);
-    $server = str_replace("ServerName ","",$desiredLine);
+    $lines = file($mamp_hosts_path);
+    $lineNumber = getLineWithString($mamp_hosts_path,$args,$lines);
+    $lineLimit = $lineNumber - 4;
+    $serverLine = lineRange($lineNumber,$lineLimit,$lines);
+    $server = str_replace("ServerName ","",$serverLine);
     $server = trim($server);
     shell_exec("open http://$server");
 }
 
-function getLineWithString($fileName, $str) {
+function getLineWithString($fileName, $str, $lines) {
     //http://stackoverflow.com/questions/9721952/search-string-and-return-line-php
-    $lines = file($fileName);
     foreach ($lines as $lineNumber => $line) {
         if (strpos($line, $str) !== false) {
             return $lineNumber;
@@ -29,13 +32,31 @@ function getLineWithString($fileName, $str) {
     return -1;
 }
 
-function grabLine($fileName,$Dline){
-    $lines = file($fileName);
+function grabLine($fileName,$Dline, $lines){
     foreach ($lines as $lineNumber => $line){
         if ($lineNumber == $Dline){
             return $line;
         }
     }
+}
+
+/**
+ * Given a range of numbers, find a particular string within that range
+ * @return string
+ */
+function lineRange($line,$lineLimit,$lines){
+    while ($line != $lineLimit)
+    {
+        $ranges[] = $lines[$line];
+        $line--;
+    }
+    foreach ($ranges as $range){
+        if (strpos($range, "ServerName") !== false) {
+            return trim($range);
+        }
+
+    }
+
 }
 
 
