ba3257bcbf38c0786c03455c06275eb1813bcaae
Khaliq Gant <khaliqgant@gmail.com>
2015-01-15 20:41:29 -0500

    remove action ability to set dir to open

--- scripts/action.php
+++ scripts/action.php
@@ -8,20 +8,14 @@ $args = explode(" ",$args);
 $ide = $workflow->get('IDE','settings/settings.plist');
 
 $file_path = $args[0];
-$action = $args[1];
-if ($action !== "*" || $action !== ".")
-{
-    $action = "/".$action;
-} else{
-    $action = " ".$action;
-}
+
 switch ($ide)
 {
     case "Sublime":
-        shell_exec("subl $file_path/$action");
+        shell_exec("subl $file_path");
         break;
     case "Macvim":
-        shell_exec("open -a $ide $file_path$action");
+        shell_exec("open -a $ide $file_path");
     break;
 }
 
